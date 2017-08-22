badf0cb5e10562d7ba27adba100225f484c5233a
동하 <mcpelee@gmail.com>
2017-08-22 21:37:14 +0900

    Add files via upload

--- MiddleMan_After2.cs
+++ MiddleMan_After2.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MiddleMan_After
+namespace MiddleMan_After2
 {
     public class 중개자
     {
         public 고객 고객 { get; set; }
+        public 주소 주소 { get; set; }
+        public 지역 지역 { get; set; }
         public string Get마지막()
         {
-            return 고객.Get주소().Get지역().Get마지막();
+            return 지역.Get마지막();
+        }
+        public 주소 Get주소()
+        {
+            return 주소;
+        }
+        public 지역 Get지역()
+        {
+            return 지역;
         }
     }
     public class 장사
@@ -24,18 +34,18 @@ namespace MiddleMan_After
     }
     public class 고객
     {
-        public 주소 주소 { get; set; }
+        public 중개자 중개자 { get; set; }
         public 주소 Get주소()
         {
-            return 주소;
+            return 중개자.주소;
         }
     }
     public class 주소
     {
-        public 지역 지역 { get; set; }
+        public 중개자 중개자 { get; set; }
         public 지역 Get지역()
         {
-            return 지역;
+            return 중개자.지역;
         }
     }
     public class 지역
